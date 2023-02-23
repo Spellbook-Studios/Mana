d220169ea4965932c77bd78bbf993990dec93de2
Sebastian Snoer <sebs@sebsa.dk>
2023-02-23 16:53:10 +0100

    Fixed name...

--- lib/src/main/java/dk/sebsa/Mana.java
+++ lib/src/main/java/dk/sebsa/Mana.java
@@ -6,11 +6,11 @@ package dk.sebsa;
 import dk.sebsa.mana.Logger;
 
 public class Mana {
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.0a";
 
     public static void logSystemInfo(Logger l) {
         l.log("----------= SYSINFO =----------");
-        l.log("EMERALD: " + VERSION);
+        l.log("MANA: " + VERSION);
         l.log("JAVA: V" + System.getProperty("java.version") + " from " + System.getProperty("java.vendor"));
         l.log("OS: " + System.getProperty("os.name") + " V" + System.getProperty("os.version"));
         l.log("DIR: " + System.getProperty("user.dir"));
