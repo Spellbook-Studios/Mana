1c67964f677b0f2a4e3e2130a45ac26360927e20
Sebastian Snoer <sebs@sebsa.dk>
2023-09-16 11:19:53 +0200

    Jenkins

--- lib/src/main/java/dk/sebsa/Mana.java
+++ lib/src/main/java/dk/sebsa/Mana.java
@@ -6,7 +6,7 @@ package dk.sebsa;
 import dk.sebsa.mana.Logger;
 
 public class Mana {
-    public static final String VERSION = "1.0a";
+    public static final String VERSION = "1.0c-SNAPSHOT";
 
     public static void logSystemInfo(Logger l) {
         l.log("----------= SYSINFO =----------");
